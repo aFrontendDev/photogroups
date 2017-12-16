d8a0cfe758c42ac44834ea41b4d7e7290b953dda
aFrontendDev <grumpydev22@gmail.com>
2017-12-16 16:55:25 +0000

    updating menus

--- scripts.js
+++ scripts.js
@@ -29,10 +29,43 @@ var siteObj = siteObj ? siteObj : {};
         document.body.classList.remove(self.menuInClass);
       } else {
         document.body.classList.add(self.menuInClass);
+        document.body.classList.remove(siteObj.userActions.menuInClass);
       }
     }
   };
 
+  siteObj.userActions = {
+    _UserMenuAction: null,
+    menuInClass: 'user-menu-in',
+    init() {
+      const self = this;
+
+      self._UserMenuAction = document.querySelector('.login-register-action');
+      if (!self._UserMenuAction) {
+        return;
+      }
+
+      self.bindEvents();
+    },
+    bindEvents() {
+      const self = this;
+
+      self._UserMenuAction.addEventListener('click', self.userActionHandler);
+    },
+    userActionHandler(e) {
+      const self = siteObj.userActions;
+
+      const isUserMenuIn = document.body.classList.contains(self.menuInClass);
+      if (isUserMenuIn) {
+        document.body.classList.remove(self.menuInClass);
+      } else {
+        document.body.classList.add(self.menuInClass);
+        document.body.classList.remove(siteObj.menu.menuInClass);
+      }
+    }
+  }
+
   //  init
   siteObj.menu.init();
+  siteObj.userActions.init();
 }());
